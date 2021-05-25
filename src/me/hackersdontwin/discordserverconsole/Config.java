b92fb3333d7a2a865be84ab6120f20ff375d5f2c
HackersDontWIn <h4ckersdontwin@gmail.com>
2021-05-25 21:49:40 +0200

    Switched from YML to JSON config

---
+++ src/me/hackersdontwin/discordserverconsole/Config.java
@@ -0,0 +1,99 @@
+package me.hackersdontwin.discordserverconsole;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Config {
+
+	DiscordServerConsole plugin;
+
+	private File file;
+	private File oldConfigFile;
+	private FileConfiguration oldConfig;
+	private JsonObject config;
+
+	public Config(DiscordServerConsole plugin) {
+		this.plugin = plugin;
+		this.file = new File(plugin.getDataFolder(), "config.json");
+		this.oldConfigFile = new File(plugin.getDataFolder(), "config.yml");
+		reload();
+	}
+
+	public void reload() {
+		try {
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+
+			if(file.exists()) {
+				this.config = plugin.getGson().fromJson(new FileReader(this.file), JsonObject.class);
+			} else {
+				setupConfig();
+			}
+
+			if(oldConfigFile.exists()) {
+				System.out.println("[DiscordServerConsole] Old configuration file found! Converting to new configuration file...");
+				oldConfig = new YamlConfiguration();
+				oldConfig.load(oldConfigFile);
+				setupConfig(oldConfig.getString("Important.Token"), oldConfig.getString("Important.ChannelID"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setupConfig() {
+		config = new JsonObject();
+		config.addProperty("token", "Token goes in here");
+		JsonArray channelIDs = new JsonArray();
+		channelIDs.add("Add the discord channel ID here!");
+		channelIDs.add("You can also add multiple channels!");
+		config.add("channelIDs", channelIDs);
+		save();
+	}
+
+	public void setupConfig(String token, String channelID) {
+		config = new JsonObject();
+		config.addProperty("token", token);
+		JsonArray channelIDs = new JsonArray();
+		channelIDs.add(channelID);
+		channelIDs.add("There's now support for multiple discord text channels!");
+		config.add("channelIDs", channelIDs);
+
+		try {
+			oldConfigFile.delete();
+		} catch (Exception e) {
+			System.out.println("[DiscordServerConsole] Conversion of old configuration file failed! Please contact plugin support on the Discord server: https://discord.gg/d3ac2tJ");
+		}
+		System.out.println("[DiscordServerConsole] Conversion of old configuration file finished!");
+		save();
+	}
+
+	public boolean save() {
+		try {
+			FileWriter fw = new FileWriter(file);
+			fw.write(plugin.getGson().toJson(config));
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public JsonObject getConfig() {
+		return config;
+	}
+
+	public String getToken() {
+		return config.get("token").getAsString();
+	}
+
+}
--- src/me/hackersdontwin/discordserverconsole/DiscordServerConsole.java
+++ src/me/hackersdontwin/discordserverconsole/DiscordServerConsole.java
@@ -1,5 +1,6 @@
 package me.hackersdontwin.discordserverconsole;
 
+import com.google.gson.Gson;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.apache.logging.log4j.LogManager;
@@ -12,11 +13,12 @@ import java.io.File;
 
 public class DiscordServerConsole extends JavaPlugin {
 
+	Gson gson = new Gson();
 
+	public Config config;
     private static Plugin plugin;
     private JDA jda;
     private LogAppender appender;
-    private FileManager fm = new FileManager();
 
     @Override
     public void onEnable() {
@@ -30,7 +32,7 @@ public class DiscordServerConsole extends JavaPlugin {
         }
         Bukkit.getLogger().info("The plugin folder was found!");
 
-        File file = new File(this.getDataFolder().getPath().toString() + "/libs");
+        File file = new File(this.getDataFolder().getPath() + "/libs");
         if(!file.exists()) {
             Bukkit.getLogger().info("Libs folder wasn't found! Creating one...");
             file.mkdir();
@@ -39,8 +41,8 @@ public class DiscordServerConsole extends JavaPlugin {
         Bukkit.getLogger().info("The libs folder was found!");
 
         Http http = new Http();
-        File jdaJarfile = new File(this.getDataFolder().getPath().toString() + "/libs/JDA-4.2.0_168-withDependencies.jar");
-        File slfJarfile = new File(this.getDataFolder().getPath().toString() + "/libs/slf4j-nop-1.7.26.jar");
+        File jdaJarfile = new File(this.getDataFolder().getPath() + "/libs/JDA-4.2.0_168-withDependencies.jar");
+        File slfJarfile = new File(this.getDataFolder().getPath() + "/libs/slf4j-nop-1.7.26.jar");
         if(!jdaJarfile.exists()) {
             Bukkit.getLogger().info("JDA API wasn't found! Downloading JDA API...");
             http.get("https://github.com/HackersDontWin/Dependencies/releases/download/DSC2.0/JDA-4.2.0_168-withDependencies.jar", this.getDataFolder().getPath().toString() + "/libs/JDA-4.2.0_168-withDependencies.jar");
@@ -53,21 +55,21 @@ public class DiscordServerConsole extends JavaPlugin {
         }
         Bukkit.getLogger().info("All the dependencies were found! Loading the config file...");
 
-        fm.loadConfigFile();
+		config = new Config(this);
 
         Bukkit.getLogger().info("The config file has been loaded! Attempting to start up the bot...");
 
         try {
-            jda = JDABuilder.createDefault(fm.getToken())
+            jda = JDABuilder.createDefault(config.getToken())
                     .build();
-            jda.addEventListener(new DiscordEvents(this.fm, this.jda));
+            jda.addEventListener(new DiscordEvents(this, this.jda));
             Bukkit.getLogger().info("The bot is now online!");
         } catch (LoginException e) {
             Bukkit.getLogger().severe("Invalid bot token! Make sure to put a bot token in the config file! Without this the plugin won't work! Shutting down the server...");
             System.exit(0);
         }
 
-        appender = new LogAppender(this.fm, this.jda);
+        appender = new LogAppender(this, this.jda);
         try {
             final org.apache.logging.log4j.core.Logger logger = (org.apache.logging.log4j.core.Logger) LogManager.getRootLogger();
             logger.addAppender(appender);
@@ -80,7 +82,6 @@ public class DiscordServerConsole extends JavaPlugin {
 
     public void onDisable() {
         plugin = null;
-        fm.saveConfig();
         appender.stop();
     }
 
@@ -91,5 +92,9 @@ public class DiscordServerConsole extends JavaPlugin {
     public JDA getJDA() {
         return jda;
     }
+
+    public Gson getGson() {
+    	return gson;
+	}
 
 }
